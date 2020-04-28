4b000bef877ba2c4788dd37f1ef2a2660d3cebb3
willtoch <wtochukwu@yahoo.com>
2020-04-29 00:43:37 +0100

    Header Added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import {Route, Switch} from 'react-router-dom';
 import './App.css';
 import Homepage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+import Header from './components/header-component/header-component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+
+
 
 function App() {
   return (
     <div >
-      <Homepage />
+      <Header />
+      <Switch>
+        <Route exact path='/' component={Homepage}/>
+        <Route path='/shops' component={ShopPage}/>
+        <Route path='/signin' component={SignInAndSignUpPage}/>
+
+      </Switch>
+  
     </div>
   );
 }
